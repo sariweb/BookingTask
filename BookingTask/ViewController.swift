6dd0864f16c0c8917934e19f30566005527fa4a5
sariweb <ariweb@ya.ru>
2023-09-12 07:20:12 +0500

    Initial Commit

---
+++ BookingTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookingTask
+//
+//  Created by Sergei on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
